e26bf21e2703ab856e424eb325516e1ff5da7798
Benjamin Mellado <melladocanihuante@gmail.com>
2021-09-12 16:41:35 -0300

    building up ComponentTypes

---
+++ source/World/ComponentTypes.hpp
@@ -0,0 +1,29 @@
+#ifndef COMPONENTTYPES_HPP
+#define COMPONENTTYPES_HPP
+
+#include <cstdint>
+#include "../Core/Common.hpp"
+
+//write the code here
+namespace Mona {
+
+	enum class EComponentType : uint8_t {
+		AudioSourceComponent,
+		ComponentTypeCount
+	};
+
+	constexpr uint8_t getComponentIndex(EComponentType type){
+		return static_cast<uint8_t>(type);
+	}
+
+	constexpr uint8_t getComponentTypeCount(){
+		return static_cast<uint8_t>(EComponentType::ComponentTypeCount);
+	}
+
+	class AudioSourceComponent;
+
+	
+	//figure out what does this line
+	template <typename ...ComponentTypes> struct DependencyList {};
+}
+#endif
